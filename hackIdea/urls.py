8e1c2dc173190675e07b7bad8a261b7147ee5ad8
jiss02 <olo5112olo@naver.com>
2020-01-18 02:34:23 +0900

    새로운view에 맞게 idea router 변경

--- hackIdea/urls.py
+++ hackIdea/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.urls import path, include
 from rest_framework import routers
-from ideas.views import IdeaViewSet
-from join.views import JoinViewSet
+# from ideas.views import IdeaViewSet
+# from join.views import JoinViewSet
 
 router = routers.DefaultRouter()
-router.register('ideas', IdeaViewSet)
-router.register('joins', JoinViewSet)
+# router.register('ideas', IdeaViewSet)
+# router.register('joins', JoinViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('ideas/', include('ideas.urls')),
 ]
