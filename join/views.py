60a3a9cf8e4dcadc5d603ccd07d444d090231a44
jiss02 <olo5112olo@naver.com>
2020-01-18 05:23:29 +0900

    join 틀 구성

--- join/views.py
+++ join/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import JoinSerializer
+from .models import Join
 
 # Create your views here.
+class JoinViewSet(viewsets.ModelViewSet):
+    queryset = Join.objects.all()
+    serializer_class = JoinSerializer
