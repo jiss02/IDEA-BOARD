55f4cdcc3c18fb3ce8be5491f054adcd32fbafe2
jiss02 <olo5112olo@naver.com>
2020-01-17 23:26:31 +0900

    ideas url 설정

--- hackIdea/urls.py
+++ hackIdea/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from ideas.views import IdeaViewSet
+
+router = routers.DefaultRouter()
+router.register('ideas', IdeaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
