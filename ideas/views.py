9c477ce8f6973c5f3117d020e8f26c3586dffb10
jiss02 <olo5112olo@naver.com>
2020-01-18 05:31:30 +0900

    idea detail page 완성

--- ideas/views.py
+++ ideas/views.py
@@ -23,7 +23,6 @@ class IdeaList(APIView):
         except Exception as err:
             print(err)
             return Response("에러발생 터미널을 확인하십시오", status=500)
-
     def get(self, req, format=None):
         try:
             queryset = Idea.objects.all().order_by('-id')
@@ -39,7 +38,6 @@ class IdeaDetail(APIView):
             return Idea.objects.get(pk=pk)
         except Idea.DoesNotExist:
             raise Http404
-
     def get(self, req, pk):
         try:
             idea = self.get_object(pk)
@@ -47,10 +45,11 @@ class IdeaDetail(APIView):
             idea.save()
             serializer = IdeaSerializer(idea)
             return Response(serializer.data)
+        except Http404:
+            return Response("없는 아이디어 입니다.", status=404)
         except Exception as err:
             print(err)
             return Response("에러발생 터미널을 확인하십시오", status=500)
-
     def put(self, req, pk):
         try:
             idea = self.get_object(pk)
@@ -59,15 +58,18 @@ class IdeaDetail(APIView):
                 serializer.save()
                 return Response(serializer.data)
             return Response(serializer.errors)
+        except Http404:
+            return Response("없는 아이디어 입니다.", status=404)
         except Exception as err:
             print(err)
             return Response("에러발생 터미널을 확인하십시오", status=500)
-
     def delete(self, req, pk):
         try:
             idea = self.get_object(pk)
             idea.delete()
             return Response("{}번 글 삭제 완료".format(pk))
+        except Http404:
+            return Response("없는 아이디어 입니다.", status=404)
         except Exception as err:
             print(err)
             return Response("에러발생 터미널을 확인하십시오", status=500)
