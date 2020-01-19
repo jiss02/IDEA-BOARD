e7a6c1ccb19276f9917e6407ff5c72293081826c
jiss02 <olo5112olo@naver.com>
2020-01-19 23:55:20 +0900

    idea str 함수 구현 및 serializer 미구현 부분 수정

--- ideas/models.py
+++ ideas/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.conf import settings 
 
 # Create your models here.
 class Idea(models.Model):
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     detail = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
--- ideas/serializer.py
+++ ideas/serializer.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import Idea
-from django.contrib.auth.models import User 
-
+from django.conf import settings 
+from django.contrib.auth import get_user_model
+from joins.models import Join
+from joins.serializer import JoinSerializer
 class IdeaSerializer(serializers.ModelSerializer):
-    
-    writer = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=True)
-
+    writer = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    joins = JoinSerializer(many=True, read_only=True, source="join_set")
     class Meta:
         model = Idea
-        fields = ('writer', 'name', 'detail','created_date', 'updated_date', 'views')
-        read_only_fields = ('writer', 'created_date', 'updated_date', 'views')
+        fields = ('writer', 'name', 'detail','created_date', 'updated_date', 'views', 'joins')
+        read_only_fields = ('writer', 'created_date', 'updated_date', 'views', 'joins')
