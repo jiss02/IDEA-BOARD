86a52791b60141760b5f1bf02c78ee568f221c88
jiss02 <olo5112olo@naver.com>
2020-01-21 10:00:56 +0900

    필요없는 url 제거 '

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken import views as authviews
 from . import views
 
 urlpatterns = [
     path('', views.usersAssign.as_view()),
     path('signup/', views.usersAssign.as_view()),
-    path('login/', views.usersLogin),
+    path('api-token-auth/', authviews.obtain_auth_token),
 ]
