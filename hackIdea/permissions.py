53ada65d387659a4a2ad85e148c04deba5bb7586
jiss02 <olo5112olo@naver.com>
2020-01-21 09:59:52 +0900

    User 회원가입을 위한 권한

---
+++ hackIdea/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+SAFE_METHODS = ['POST']
+
+class UserPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if (request.method in SAFE_METHODS):
+            return True
+        return False
