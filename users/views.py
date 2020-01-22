bb832a2ca1e7b15078ef6e0c2821f03c26fd066c
jiss02 <olo5112olo@naver.com>
2020-01-22 13:54:43 +0900

    user views 로그인 구현에 맞게 조정

--- users/views.py
+++ users/views.py
@@ -5,9 +5,14 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import UserSerializer
-# from .models import User
-
+from hackIdea.permissions import UserPermission
+# 에러
+from hackIdea import errors
+# 로직
 class usersAssign(APIView):
+    
+    permission_classes = (UserPermission,)
+
     def get(self, req):
         try:
             User = get_user_model()
@@ -19,13 +24,17 @@ class usersAssign(APIView):
             return Response("에러발생 터미널을 확인하십시오", status=500)
     def post(self, req):
         try:
-            return Response(serializer.data)
-            return Response(serializer.error)
+            eights = ["지현이","이주형","최정연", "유지연"]
+            if req.data["username"] not in eights:
+                raise errors.NotEights
+            serializer = UserSerializer(data=req.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors)
+        except errors.NotEights:
+            return Response("8기 멋사 구성원이 아닙니다")
         except Exception as err:
             print(err)
             return Response("에러발생 터미널을 확인하십시오", status=500)
 
-# 로그인 회원가입 
-@api_view(['POST'])
-def usersLogin(req):
-    return Response("로그인")
