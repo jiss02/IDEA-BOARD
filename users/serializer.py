9629baa0a809f51e95bf36844cca88aeb0eb3a7d
jiss02 <olo5112olo@naver.com>
2020-01-22 13:53:50 +0900

    user model, serializer 로그인 구현에 맞게 조정

--- users/serializer.py
+++ users/serializer.py
@@ -14,4 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         User = get_user_model()
         model = User
-        fields = ('username', 'email', 'ideas', 'joins')
+        fields = ('username', 'email', 'password','ideas', 'joins')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
